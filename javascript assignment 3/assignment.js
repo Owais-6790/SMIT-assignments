7b345a8d80ef041707200d2fe9500b0e419b588d
Owais-6790 <owaisrehman6790@gmail.com>
2025-08-24 03:53:11 +0500

    loops

---
+++ javascript assignment 3/assignment.js
@@ -0,0 +1,158 @@
+// Question no 1
+
+// age = prompt("Enter your age")
+
+// if (age < 13) {
+//     console.log("Child")
+// } else if (age >= 13 && age <= 19) {
+//     console.log("teenager")
+// } else {
+//     console.log("adult")
+// }
+
+
+
+// Question no 2
+
+// User_Number = prompt("Enter your Number")
+
+// if (User_Number % 2 === 0 && User_Number % 3 === 0) {
+//     console.log("divisible by both")
+// } else {
+//     console.log("Not divisible by both")
+// }
+
+
+
+
+// Question no 3
+
+// password = prompt("Enter your password")
+
+// if (password === "saylani123") {
+//     console.log("Access granted")
+// } else {
+//     console.log("Access not granted")
+// }
+
+
+
+
+// Question no 4
+
+// Number_1 = prompt("enter first number") 
+// Number_2 = prompt("enter Second number")
+
+// if (Number_1 > Number_2) {
+//     console.log(Number_1)
+// } else {
+//     console.log (Number_2)
+// }
+
+
+
+
+// Question no 5
+
+// five_Colors = ["Red","blue","purple","black"," white"]
+
+// alert( five_Colors[0] + five_Colors[4] )
+
+
+
+
+
+// // Question no 6
+
+// fruits = ["banana","grapes","apples"]
+
+// fruits.push("mango")
+
+// console.log(fruits)
+
+
+
+
+// Question no 7
+
+// city = ["victoria","melbourne","Sydney"]
+
+// city.shift()
+
+// console.log(city)
+
+
+
+
+
+// Question no 8
+
+// Numbers = [10,20,30,40,50]
+
+// Numbers.splice(2,1)
+
+// console.log(Numbers)
+
+
+
+
+
+
+// Question no 9
+
+// Numbers = [100,200,300,400,500]
+
+// slice_Numbers = Numbers.slice(1,4)
+
+// console.log(slice_Numbers)
+
+
+
+
+
+// Question no 10
+
+// Maths_marks = prompt("maths marks")
+// English_marks = prompt("english marks")
+// computer_marks = prompt("computer marks")
+
+// Subjects_marks = [ Maths_marks , English_marks , computer_marks ]
+// console.log(Subjects_marks)
+
+// Subjects_marks[0] = Number(Subjects_marks[0])
+// Subjects_marks[1] = Number(Subjects_marks[1])
+// Subjects_marks[2] = Number(Subjects_marks[2])
+
+// total = (Subjects_marks[0]+Subjects_marks[1]+Subjects_marks[2])
+// console.log(total)
+
+// average =((Subjects_marks[0]+Subjects_marks[1]+Subjects_marks[2])/3)
+// console.log(average)
+
+
+
+
+
+// Question no 11
+
+// User_month= prompt("enter your month number (1-12)")
+
+// if (User_month >= 1 && User_month <= 3) {
+//     console.log("Winter")
+// } else if (User_month >= 4 && User_month <= 6) {
+//     console.log("Spring")
+// } else if (User_month >= 7 && User_month <= 9) {
+//     console.log("summer")
+// } else {
+//     console.log("Autumn")
+// }
+
+
+
+
+
+// Question no 12
+
+for (let i = 2; i < 20; i++) {
+  console.log(i+2);
+}
